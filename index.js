5529873f9f7ef1c053ae25a81df18750d0ff8433
Miguel Gomez <andresmgomez23@gmail.com>
2022-04-29 11:17:08 -0400

    Add sample restaurant object to Firestore

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ const serviceAccount = require('./credentials.json');
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 });
+
+const db = admin.firestore();
+// const newObject = new Object();
+
+const restaurant = {
+	name: 'Mister 01',
+	address: {
+		street: '355 North Federal Highway',
+		office: 'Suite #5',
+		city: 'Boca Raton',
+		state: 'FL',
+		zipCode: '33432',
+		phone: '(786) 677-2903',
+	},
+	online: {
+		rating: 4.9,
+	},
+};
+// console.log(db);
+
+const addRestaurant = db
+	.collection('restaurants')
+	.add(restaurant)
+	.then(doc => console.log('Created restaurant', doc.id))
+	.catch(err => console.error(err));
+
+// console.log(addRestaurant);
